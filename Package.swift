eb3c4c58ec4e69a0e209b47f142c50e99d970ec9
Nick Beadman <nbeadman@gmail.com>
2021-09-11 09:55:38 -0700

    Renamed SJSegmentedScrollView

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "SJSegmentedViewController",
+    name: "SJSegmentedScrollView",
     platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SJSegmentedViewController",
-            targets: ["SJSegmentedViewController"]),
+            name: "SJSegmentedScrollView",
+            targets: ["SJSegmentedScrollView"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,8 +20,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SJSegmentedViewController",
+            name: "SJSegmentedScrollView",
             dependencies: [],
-			path: "SJSegmentedScrollView/Classes")
+			path: "SJSegmentedScrollView")
     ]
 )
